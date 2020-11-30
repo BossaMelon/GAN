bc4694b20cf14f8ba4f317a053d338bdbafaf799
BossaMelon <wyh453255505@gmail.com>
2020-11-30 04:38:07 +0100

    first commite

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,18 @@
+import os
+
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
+data_root = './data'
+
 
 def get_dataloader(batch_size):
+    if not os.path.exists(data_root):
+        os.makedirs(data_root)
+
     dataloader = DataLoader(
-        MNIST(root='./data', download=True, transform=transforms.ToTensor()),
+        MNIST(root=data_root, download=True, transform=transforms.ToTensor()),
         batch_size=batch_size,
         shuffle=True)
     return dataloader
