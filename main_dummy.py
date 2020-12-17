3c11e03399d011201da82d478b0e21dae51cee28
BossaMelon <wyh453255505@gmail.com>
2020-12-17 05:45:02 +0100

    add controllable gan

--- main_dummy.py
+++ main_dummy.py
@@ -1,7 +1,9 @@
 from experiments import *
-
+from models.model_controllable_gan import train_classifier
 
 #gan.run_experiment(1)
 # dcgan.run_experiment(1)
 # wgangp.run_experiment(100)
-cgan.run_experiment(200)
+# cgan.run_experiment(200)
+
+train_classifier('classifier.pt')
